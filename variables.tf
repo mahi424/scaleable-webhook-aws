0989c54e5eb31e50dcea466fcdd3bbb9e928d162
Mahaveer Chouhan <mahaveer.chouhan@primathon.in>
2023-07-19 11:53:36 +0530

    [ADD] initial

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "us-east-1" # Replace with your desired region
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "api_gateway_id" {
+  type = string
+}
